0f9638bf429ac03bab42892d0f534b2861b01b5c
fixopen <fixopen@gmail.com>
2015-04-09 14:20:29 +0800

    download return length and content

--- api/BinaryDownloader.php
+++ api/BinaryDownloader.php
@@ -11,9 +11,10 @@ trait BinaryDownloader
         $file = fopen($filename, 'rb');
         fseek($file, 0, SEEK_END);
         $length = ftell($file);
+        fseek($file, 0, SEEK_SET);
         $content = fread($file, $length);
         fclose($file);
-        return $content;
+        return array('length' => $length, 'content' => $content);
     }
 
     public function downloadSlice($type, $offset, $count)
