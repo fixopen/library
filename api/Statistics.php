2e8c44f3d1f8c5e8a7bcab20c194cca5af501745
DuanShengpeng <duanshp@chineseall.com>
2015-06-15 14:03:13 +0800

    remark debug log for statistics

--- api/Statistics.php
+++ api/Statistics.php
@@ -17,7 +17,7 @@ trait Statistics
         //print 'filter is<br />';
         //print 'filter is ' . $filter . '<br />';
         if (!empty($filter)) {
-            print 'filter is ' . $filter . '<br />';
+            //print 'filter is ' . $filter . '<br />';
             $whereClause = ' WHERE ' . self::ConvertJsonToWhere($filter);
         }
         //$whereClause = ' WHERE ' . $filter;
@@ -25,7 +25,7 @@ trait Statistics
         //    $whereClause = '';
         //}
         $query = 'SELECT ' . $calc . ' FROM ' . self::Mark(self::$tableName) . $whereClause;
-        print $query . '<br />';
+        //print $query . '<br />';
         $r = Database::GetInstance()->query($query);
         if ($r) {
             foreach ($r as $row) {
